e202692693213346445db0ce7407a75eef03f7b3
Harold Almon <haroldemail@gmail.com>
2017-09-06 18:44:16 -0500

    Remove return variables

--- ProjectEuler/p000/P001_MultiplesOf3And5.R
+++ ProjectEuler/p000/P001_MultiplesOf3And5.R
@@ -8,16 +8,14 @@
 # The formula includes n, the problem does not.
  calculateSumBelowLimit <- function(upperLimit, divisor) {
   n <- floor((upperLimit - 1)/divisor)
-  result <- floor(divisor * n * ( n + 1 ) / 2) 
-  result
+  floor(divisor * n * ( n + 1 ) / 2) 
 }
 
 # Strategy: Simple Mathematics. Time complexity is O(1)
 multiplesOf3And5 <- function(upperLimit)  {
-    result <- calculateSumBelowLimit(upperLimit, 3) +
+    calculateSumBelowLimit(upperLimit, 3) +
       calculateSumBelowLimit(upperLimit, 5) -
       calculateSumBelowLimit(upperLimit, 15);
-    result
 }
 
 multiplesOf3And5(1000)
