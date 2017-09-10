d0e48ea767cc08a1bfb467dc12e07655105d1c0c
Harold Almon <haroldemail@gmail.com>
2017-09-10 13:31:35 -0500

    Minor code change

--- ProjectEuler/p000/P002_EvenFibonacciNumbers.R
+++ ProjectEuler/p000/P002_EvenFibonacciNumbers.R
@@ -3,9 +3,9 @@ fiboSumImpl <- function(fibo1, fibo2, total, upperLimit) {
   nextFibo = fibo1 + fibo2;
   if ( nextFibo < upperLimit) {
     if(nextFibo %% 2 == 0)
-      return (fiboSumImpl(fibo2, nextFibo, total + nextFibo, upperLimit))
+      fiboSumImpl(fibo2, nextFibo, total + nextFibo, upperLimit)
     else
-      return (fiboSumImpl(fibo2, nextFibo, total, upperLimit))
+      fiboSumImpl(fibo2, nextFibo, total, upperLimit)
   }
   else
     total
@@ -16,5 +16,3 @@ fiboSum <- function(upperLimit) {
 }
 
 fiboSum(4000000)
-  
-
