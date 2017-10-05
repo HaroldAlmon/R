d10e226591f7c6d6bed26bbb603343b332cf5a58
Harold Almon <haroldemail@gmail.com>
2017-10-05 20:54:12 +0800

    Initial version

---
+++ ProjectEuler/p000/P009_PythagoreanTriplet.R
@@ -0,0 +1,47 @@
+# Strategy: Brute Force.
+  pythagoreanTriplet <- function(tripletSum) {
+    enumerateAllXValues(tripletSum)
+  }
+  
+  enumerateAllXValues <- function(tripletSum) {
+    tripletProduct = 0;
+    for (x in 3:tripletSum - 2) {
+      tripletProduct = enumerateAllYValues(tripletSum, x);
+      if (tripletProduct !=  0)
+        break;
+    }
+    tripletProduct
+  }
+  
+  enumerateAllYValues <- function(tripletSum, x) {
+    isDebug = T
+    tripletProduct = 0
+    
+    for (y in x + 1 : tripletSum - 2) {
+      # Choose an r such that r + x + y = 1000
+      r = tripletSum - x - y
+      if ( isPythogoreanTriplet(x, r, y) ) {
+        tripletProduct = x*y*r
+        if (isDebug) {
+          str <- sprintf("Triplet (x, y, r) = (%d,%d,%d)\n", x,y,r)
+          cat(str, "\n")
+        }
+      }
+    }
+    tripletProduct
+  }
+  
+  isPythogoreanTriplet<- function(x, r, y) {
+    x*x + y*y == r*r
+  }
+  
+  PythagoreanTriplet <-function() {
+    result = pythagoreanTriplet(1000);
+    cat(result, "\n")
+    if (result == 31875000) {
+      cat("Pass", "\n")
+    } else 
+      cat("Fail", "\n")
+  }
+  
+  PythagoreanTriplet()
